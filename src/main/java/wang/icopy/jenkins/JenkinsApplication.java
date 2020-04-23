76ee43fb6a7833c15b9c8a0c8d653b28e915165d
lizhengguang <zzlxxi@163.com>
2020-04-23 12:24:35 +0800

    first commit. hello world

--- src/main/java/wang/icopy/jenkins/JenkinsApplication.java
+++ src/main/java/wang/icopy/jenkins/JenkinsApplication.java
@@ -12,7 +12,7 @@ public class JenkinsApplication {
 
   @GetMapping("/")
   public String index() {
-    return "Hello World, Hello Jenkins";
+    return "Hello Jenkins";
   }
 
   public static void main(String[] args) {
